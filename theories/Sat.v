3451d2ff085946ef84f36285d4a6b7ec05cd45d3
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2022-02-14 16:59:24 -0800

    Struggle with sat

--- theories/Sat.v
+++ theories/Sat.v
@@ -58,17 +58,38 @@ Import List.ListNotations.
 
 Notation "'do' x <- e0 ; e1" := (List.flat_map (fun x => e1) e0) (x ident, at level 200, left associativity).
 
+Fixpoint mon {A B} (x: list A) (y: list B): list _ :=
+  match x with
+  | cons H T => List.map (fun y' => (H, y')) y ++ mon T y
+  | _ => nil
+  end.
+Infix ">*<" := mon (at level 30).
+
+Lemma mon_nil:
+  forall {A B} (x: list A), x >*< @nil B = nil.
+Proof using.
+  intros ? ? x.
+  induction x.
+  1: reflexivity.
+  cbn.
+  rewrite IHx.
+  reflexivity.
+Qed.
+
+Definition app {A B} (f: list (A -> B)) x :=
+  List.map (fun '(f, x) => f x) (f >*< x).
+
+Infix "<*>" := app (at level 30).
+
 Fixpoint generate (t: type): list term :=
   match t with
   | t_unit => [v_tt]
   | A * B =>
-      do v0 <- generate A ;
-      do v1 <- generate B ;
-      [ v_fanout v0 v1 ]
+      [ fun v0 v1 => v_fanout v0 v1 ] <*> generate A <*> generate B
   end%list.
 
 (* FIXME normalize *)
-Fixpoint search (σ: Map.map term) E: list term :=
+Function search (σ: Map.map term) E: list term :=
   match E with
   | E_var x => if Map.find x σ is Some v then [v] else []
 
@@ -79,30 +100,39 @@ Fixpoint search (σ: Map.map term) E: list term :=
       [v_fanout v0 v1]
 
   | E_app E E' =>
-      do tuple <- search σ E ;
-      do v0 <- search σ E' ;
-      do ab <- (if tuple is v_fanout a b then [(a, b)] else []) ;
-      if eq_term v0 (fst ab) then [snd ab] else []
+      do f <- search σ E ;
+      do x <- search σ E' ;
+      if f is v_fanout a b
+      then
+        if eq_term x a
+        then
+          [b]
+        else []
+      else []
 
   | E_tt => [v_tt]
   | E_step E E' =>
       do p <- search σ E ;
-      do _ <- (if p is v_tt then [tt] else []) ;
-      search σ E'
+      do x <- search σ E' ;
+      if p is v_tt then [x] else []
 
   | E_fanout E E' =>
      do x <- search σ E ;
      do y <- search σ E' ;
      [v_fanout x y]
+
   | E_let x y E E' =>
       do tuple <- search σ E ;
       do ab <- (if tuple is v_fanout a b then [(a, b)] else []) ;
       search (Map.add x (fst ab) (Map.add y (snd ab) σ)) E'
   end%list.
 
+
+Definition matches σ E v := List.existsb (fun y => if eq_term v y then true else false) (search σ E).
+
 Lemma Forall_concat:
   forall {A} p (x: list (list A)), List.Forall (List.Forall p) x -> List.Forall p (List.concat x).
-Proof.
+Proof using.
   all: intros.
   induction H.
   1: econstructor.
@@ -115,9 +145,21 @@ Proof.
   auto.
 Qed.
 
+Lemma Forall_map:
+  forall {A B} p (f: A -> B) x, List.Forall (fun x => p (f x)) x -> List.Forall p (List.map f x).
+Proof using.
+  all: intros ? ? ? ? ? p.
+  induction p.
+  1: constructor.
+  cbn.
+  constructor.
+  1: auto.
+  auto.
+Qed.
+
 Lemma Forall_concat':
   forall {A} p (x: list (list A)), List.Forall p (List.concat x) -> List.Forall (List.Forall p) x.
-Proof.
+Proof using.
   intros ? ? x.
   induction x.
   all: intros.
@@ -148,7 +190,7 @@ Qed.
 
 Theorem Forall_flat_map:
   forall {A B} p (f: A -> list B) x, List.Forall (fun x => List.Forall p (f x)) x -> List.Forall p (List.flat_map f x).
-Proof.
+Proof using.
   intros.
   rewrite List.flat_map_concat_map.
   apply Forall_concat.
@@ -161,7 +203,7 @@ Qed.
 
 Theorem Forall_flat_map':
   forall {A B} p (f: A -> list B) x, List.Forall p (List.flat_map f x) -> List.Forall (fun x => List.Forall p (f x)) x.
-Proof.
+Proof using.
   intros ? ? ? ? ?.
   induction x.
   1: constructor.
@@ -180,10 +222,8 @@ Proof.
 Qed.
 
 Theorem search_sound:
-  forall σ E, List.Forall (sat σ E) (search σ E).
-Proof.
-  intros σ E.
-  generalize dependent σ.
+  forall σ E x, matches σ E x = true -> sat σ E x.
+Proof using.
   admit.
 Admitted.
 
